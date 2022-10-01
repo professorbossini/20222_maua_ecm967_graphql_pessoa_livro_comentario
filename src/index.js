7f1b2da821b3073b6efbcb243ee84f58744573fa
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-01 07:57:16 -0300

    started subscription stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createServer } from '@graphql-yoga/node'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import Livro from './resolvers/Livro'
 import Pessoa from './resolvers/Pessoa'
 import Comentario from './resolvers/Comentario'
@@ -12,6 +13,7 @@ import * as fs from 'fs';
 const resolvers = {
   Query: Query,
   Mutation: Mutation,
+  Subscription,
   Livro: Livro,
   Pessoa: Pessoa,
   Comentario: Comentario
