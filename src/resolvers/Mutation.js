e7f68c5b59f9468f7af972e77410fae676485612
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-30 23:40:31 -0300

    mutation: fim da apostila 06-01

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -9,6 +9,16 @@ const Mutation = {
     ctx.db.pessoas.push(pessoa)
     return pessoa
   },
+  atualizarPessoa (parent, args, {db}, info){
+
+    const pessoa = db.pessoas.find((p) => p.id === args.id);
+
+    if (!pessoa)
+      throw new Error ("Pessoa não existe");
+    
+    Object.assign(pessoa, { nome: args.pessoa.nome || pessoa.nome, idade: args.pessoa.idade || pessoa.idade});
+    return pessoa;
+  },
   inserirLivro (parent, args, ctx, info){
     const autorExiste = ctx.db.pessoas.some(p => p.id === args.autor)
     if (!autorExiste)
@@ -21,6 +31,14 @@ const Mutation = {
     }
     ctx.db.livros.push(livro)
     return livro
+  },
+  atualizarLivro (parent, {id, livro}, ctx, info){
+    const {db} = ctx;
+    const livroExistente = db.livros.find (l => l.id === id);
+    if (!livroExistente)
+      throw new Error ("Livro não existe")
+    Object.assign(livroExistente, { titulo: livro.titulo || livroExistente.titulo, edicao: livro.edicao || livroExistente.edicao});
+    return livroExistente;
   },
   inserirComentario(parent, args, ctx, info){
     if (!ctx.db.pessoas.some(p => p.id === args.comentario.autor) || !ctx.db.livros.some(l => l.id === args.comentario.livro)){
@@ -36,6 +54,17 @@ const Mutation = {
     ctx.db.comentarios.push(comentario)
     return comentario
   },
+  atualizarComentario (parent, {id, comentario}, {db}, info){
+    const comentarioExistente = db.comentarios.find (l => l.id === id);
+    if (!comentarioExistente)
+      throw new Error ("Comentario não existe")
+    Object.assign (comentarioExistente, {
+      texto: comentario.texto || comentarioExistente.texto,
+      nota: comentario.nota || comentarioExistente.nota
+    });
+    return comentarioExistente;
+
+  }
 }
 
 export default Mutation
